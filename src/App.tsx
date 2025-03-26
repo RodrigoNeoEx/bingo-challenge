fcfb6a568dcd26b73837bc9038e2c73a93713e26
NeoEx <draja.acc@gmail.com>
2025-03-26 15:10:48 -0300

    feat: created container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from './components/container/Container';
 
 function App() {
   return (
-    <div className='bg-red-500 text-white text-center p-4'>
+    <Container >
       <h1>React App</h1>
-    </div>
+    </Container>
   );
 }
 
---
+++ src/components/container/Container.tsx
@@ -0,0 +1,9 @@
+const Container = ({children}: any) => {
+  return (
+    <div className="bg-blue-100 text-white text-center w-full h-screen">
+      {children}
+    </div>
+  );
+}
+
+export default Container;
